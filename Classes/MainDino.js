373481471597a8b802e0246f0648689dc7f2d505
Anderson Miranda Vesguerber <avesguerber@gmail.com>
2020-07-04 19:19:10 -0300

    bug fix

--- Classes/MainDino.js
+++ Classes/MainDino.js
@@ -14,7 +14,7 @@ function CreateNewDino() {
 
     obj.imgJumping = new Image();
     obj.imgJumping.src = "sprites/dino/dinoframe2.png";
-    
+
     obj.jumpforce = 20;
     obj.currentJumpForce = 0;
 
@@ -23,8 +23,8 @@ function CreateNewDino() {
 
     obj.akPosX = 15;
     obj.akPosY = 17;
-    
-    
+
+
     obj.imgAkShot = new Image();
     obj.imgAkShot.src = "sprites/dino/shot.png";
 
@@ -40,9 +40,9 @@ function CreateNewDino() {
     obj.gravity = 1;
     obj.onGround = true;
 
-    obj.update = function(delta){
+    obj.update = function(delta) {
 
-        if (obj.state == "jumping"){
+        if (obj.state == "jumping") {
 
             obj.velocityY += obj.gravity * delta / 1000;
             obj.velocityY += obj.gravity * delta / 1000;
@@ -51,7 +51,7 @@ function CreateNewDino() {
             obj.velocityY += obj.gravity * delta / 1000;
             obj.velocityY += obj.gravity * delta / 1000;
 
-            
+
             obj.posY += obj.velocityY;
 
 
@@ -59,14 +59,13 @@ function CreateNewDino() {
 
 
 
-            
-            if(obj.posY > 140.0)
-            {
-                obj.posY  = 140.0;
+
+            if (obj.posY > 140.0) {
+                obj.posY = 140.0;
                 obj.velocityY = 0.0;
                 obj.onGround = true;
                 obj.state = "running";
-            } 
+            }
 
         }
 
@@ -74,50 +73,49 @@ function CreateNewDino() {
 
     obj.render = function(ctx, delta) {
 
-        if (obj.state == "running" || obj.state == "jumping"){
-            
-            obj.frame = obj.animation.getAnimationFrame(delta / 1000);
-            ctx.drawImage(obj.frame, obj.posX, obj.posY );
+        if (obj.state == "running" || obj.state == "jumping") {
 
-            
+            obj.frame = obj.animation.getAnimationFrame(delta / 1000);
+            ctx.drawImage(obj.frame, obj.posX, obj.posY);
+
+
 
             //alert(obj.frame.src);
 
-            if (obj.frame.src.includes( "prites/dino/dinoframe2.png") ){
+            if (obj.frame.src.includes("prites/dino/dinoframe2.png")) {
                 obj.offset = -1;
             } else {
                 obj.offset = 0;
             }
 
-            ctx.drawImage(obj.imgAk, obj.posX + obj.akPosX ,  obj.offset + obj.posY + obj.akPosY );
+            ctx.drawImage(obj.imgAk, obj.posX + obj.akPosX, obj.offset + obj.posY + obj.akPosY);
 
-            if (obj.shotSpriteTimeCounter > 0){
-                ctx.drawImage(obj.imgAkShot, obj.posX + obj.akPosX + obj.akShotX, obj.offset + obj.posY + obj.akPosY + obj.akShotY );
+            if (obj.shotSpriteTimeCounter > 0) {
+                ctx.drawImage(obj.imgAkShot, obj.posX + obj.akPosX + obj.akShotX, obj.offset + obj.posY + obj.akPosY + obj.akShotY);
                 obj.shotSpriteTimeCounter -= delta / 1000;
             }
         }
 
     }
 
-    obj.jump = function(){
+    obj.jump = function() {
 
-        if(obj.state != "jumping")
-        {
-        
+        if (obj.state != "jumping") {
+
             obj.state = "jumping";
             obj.velocityY = -4.0;
-            
+
         }
     }
 
-    obj.shot = function(){
+    obj.shot = function() {
 
         obj.shotSpriteTimeCounter = obj.shotSpriteTime;
 
-    }
 
 
-    
+
+
 
         ctx.drawImage(obj.animation.getAnimationFrame(delta / 1000), 40, 135);
 
