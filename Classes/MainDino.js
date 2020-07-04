dc870dd6fb26cb97992d3d6687b2c817a0e25fed
Bruno Sabino <brunopoor@gmail.com>
2020-07-04 15:44:08 -0300

    Main dinossaur animation

---
+++ Classes/MainDino.js
@@ -0,0 +1,24 @@
+
+function CreateNewDino(){
+
+
+    var obj = {};
+
+
+
+    obj.animation = CreateNewAnimation(
+        ["sprites/dino/dinoframe2.png","sprites/dino/dinoframe1.png","sprites/dino/dinoframe2.png","sprites/dino/dinoframe3.png"],
+        [.1,.03,.1,.03]);
+
+
+    obj.render = function(ctx ,delta){
+
+        ctx.drawImage(obj.animation.getAnimationFrame(delta / 1000), 40,140 );
+
+    }
+
+    
+
+    return obj;
+
+}
