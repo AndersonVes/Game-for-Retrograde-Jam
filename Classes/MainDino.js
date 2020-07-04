25f9488af59803310a421a8ed8fe5b0155d337c6
Anderson Miranda Vesguerber <avesguerber@gmail.com>
2020-07-04 16:31:44 -0300

    menu, commands and dino position

--- Classes/MainDino.js
+++ Classes/MainDino.js
@@ -1,5 +1,4 @@
-
-function CreateNewDino(){
+function CreateNewDino() {
 
 
     var obj = {};
@@ -7,17 +6,16 @@ function CreateNewDino(){
 
 
     obj.animation = CreateNewAnimation(
-        ["sprites/dino/dinoframe2.png","sprites/dino/dinoframe1.png","sprites/dino/dinoframe2.png","sprites/dino/dinoframe3.png"],
-        [.1,.03,.1,.03]);
+        ["sprites/dino/dinoframe2.png", "sprites/dino/dinoframe1.png", "sprites/dino/dinoframe2.png", "sprites/dino/dinoframe3.png"], [.1, .03, .1, .03]);
 
 
-    obj.render = function(ctx ,delta){
+    obj.render = function(ctx, delta) {
 
-        ctx.drawImage(obj.animation.getAnimationFrame(delta / 1000), 40,140 );
+        ctx.drawImage(obj.animation.getAnimationFrame(delta / 1000), 40, 135);
 
     }
 
-    
+
 
     return obj;
 
