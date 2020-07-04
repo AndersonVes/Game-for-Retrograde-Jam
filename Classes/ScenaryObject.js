708b03f7e73b541d5bf5f60ca9a0cd25500ddebc
Anderson Miranda Vesguerber <avesguerber@gmail.com>
2020-07-04 14:07:21 -0300

    commit

--- Classes/ScenaryObject.js
+++ Classes/ScenaryObject.js
@@ -1,24 +1,24 @@
 //This type of object won't collide with anything in the game
 
-function CreateNewScenaryObject(posX, posY,sizeX, sizeY, imagePath, canvasWidth) {
+function CreateNewScenaryObject(posX, posY, sizeX, sizeY, imagePath, canvasWidth) {
 
     var obj = {}
 
     obj.sizeX = sizeX;
     obj.sizeY = sizeY;
 
-    
+
     obj.objImage = [];
     obj.objImage.length = imagePath.length;
 
-    for (var i = 0; i < imagePath.length; i++ ){
+    for (var i = 0; i < imagePath.length; i++) {
         obj.objImage[i] = new Image();
         obj.objImage[i].src = imagePath[i];
     }
 
-    obj.imageIndex = Math.floor(Math.random() * imagePath.length);  
+    obj.imageIndex = Math.floor(Math.random() * imagePath.length);
 
-alert(obj.objImage[obj.imageIndex].src);
+    alert(obj.objImage[obj.imageIndex].src);
 
     obj.posX = posX;
     obj.posY = posY;
@@ -29,14 +29,14 @@ alert(obj.objImage[obj.imageIndex].src);
      * Move the object in the X axis
      * @param {any} speed 
      */
-    obj.move = function (speed) {
+    obj.move = function(speed) {
 
-        posX -=  speed;
+        posX -= speed;
 
-        
+
         if (posX < -sizeX) {
-            posX = canvasWidth ;//+ Math.floor(Math.random() * 100);  
-            obj.imageIndex = Math.floor(Math.random() * imagePath.length);  
+            posX = canvasWidth; //+ Math.floor(Math.random() * 100);  
+            obj.imageIndex = Math.floor(Math.random() * imagePath.length);
         }
 
     }
@@ -45,9 +45,9 @@ alert(obj.objImage[obj.imageIndex].src);
      * Render the object image inside a canvas
      * @param {any} ctx The canvas to render
      */
-    obj.render = function (ctx) {
+    obj.render = function(ctx) {
 
-        
+
         ctx.drawImage(obj.objImage[obj.imageIndex], posX, posY);
 
     }
