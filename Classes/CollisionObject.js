b88faf18d4b42ee0f964886ba17e92b0fc5b1f59
Anderson Miranda Vesguerber <avesguerber@gmail.com>
2020-07-06 21:19:03 -0300

    new villain

--- Classes/CollisionObject.js
+++ Classes/CollisionObject.js
@@ -1,11 +1,9 @@
-
-
-function CreateNewCollisionObject( postionY, width, height,  canvasWidth, imagePath ){
+function CreateNewCollisionObject(postionY, width, height, canvasWidth, imagePath) {
 
 
     obj = {};
 
-    obj.posX =  canvasWidth;
+    obj.posX = canvasWidth;
     obj.posY = postionY;
 
     obj.sizeX = width;
@@ -14,29 +12,28 @@ function CreateNewCollisionObject( postionY, width, height,  canvasWidth, imageP
 
     obj.image = new Image();
     obj.image.src = imagePath;
-    alert(obj.image.src);
 
 
 
     obj.move = function(speed, delta) {
 
-        if (obj.canRespawn){
+        if (obj.canRespawn) {
             obj.posX -= speed * delta / 1000;
             console.log(obj.posY + " " + obj.posX)
             if (obj.posX < -obj.sizeX) {
-               obj.canRespawn = true;   
+                obj.canRespawn = true;
             }
         }
 
     }
 
-    obj.checkColision = function(dinoPosX, dinoPosY, dinoWidth, dinoHeight){
+    obj.checkColision = function(dinoPosX, dinoPosY, dinoWidth, dinoHeight) {
 
-        if (obj.canRespawn == false){
-            
+        if (obj.canRespawn == false) {
+
             if (dinoPosX < obj.posX + obj.sizeX &&
                 dinoPosX + dinoWidth > obj.posX &&
-                dinoPosY < obj.posY + obj.sizeY  &&
+                dinoPosY < obj.posY + obj.sizeY &&
                 dinoPosY + dinoHeight > obj.posY) {
 
                 console.log("Dino colidiu");
@@ -50,11 +47,11 @@ function CreateNewCollisionObject( postionY, width, height,  canvasWidth, imageP
     }
 
 
-    obj.render = function(ctx){
+    obj.render = function(ctx) {
 
         //if (obj.canRespawn == false){
-            ctx.drawImage(obj.image, obj.posX, obj.posY);
-       // }
+        //ctx.drawImage(obj.image, 0, 0);
+        // }
     }
 
     return obj;
