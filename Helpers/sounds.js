90a1cd4eac96ee279702de495b0b6099c87987d7
Anderson Miranda Vesguerber <avesguerber@gmail.com>
2020-07-10 17:16:57 -0300

    pause audios

---
+++ Helpers/sounds.js
@@ -0,0 +1,8 @@
+function pauseAudios(final) {
+    jumpAudio.pause();
+    shootAudio.pause();
+    collisionAudio.pause();
+    bgAudio.pause();
+    enemyDieAudio.pause();
+    wavePassed.pause();
+}
